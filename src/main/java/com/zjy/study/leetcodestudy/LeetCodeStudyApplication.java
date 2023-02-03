efc3927dbf1c00633290d954b3e84bf80811ac36
zjy <956541455@qq.com>
2023-02-03 10:10:03 +0800

    这就是我的学习仓库了‘
    git commit -m 这就是我的学习仓库了‘

---
+++ src/main/java/com/zjy/study/leetcodestudy/LeetCodeStudyApplication.java
@@ -0,0 +1,13 @@
+package com.zjy.study.leetcodestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCodeStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetCodeStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zjy/study/leetcodestudy/LeetCodeStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjy.study.leetcodestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodeStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
