8253f2c7847bac47934fdb5c0741022811bc6824
zjy <956541455@qq.com>
2023-02-23 16:48:43 +0800

    2023/2/23

--- src/main/java/com/zjy/study/leetcodestudy/node/JavaBase.md
+++ src/main/java/com/zjy/study/leetcodestudy/node/JavaBase.md
@@ -17,3 +17,52 @@ HashMap:
 初始化大小默认16,2倍扩容,负载因子0.75
 JDK1.7采用数组+链表存储
 JDK1.8采用数组+链表+红黑树存储(链表长度超过阈值[8])
+
+jsp,servlet?:
+servlet是服务端的java程序,担当客户端和服务端的中间层
+jsp是java服务器页面,本质是简化的servlet设计,目的是将表示逻辑从servlet中分离出来
+JVM只能识别java代码,不能识别jsp,jsp编译成JVM能识别的servlet
+jsp有内置对象,servlet没有内置对象
+
+jsp内置对象?:
+1.pageContext,页面上下文对象
+2.request
+3.response
+4.session
+5.application,应用程序对象
+6.page,JSP本身
+7.exception
+8.out,用于在web浏览器内输出信息,管理应用服务器上的缓冲区,作用域page
+9.config,获取服务器的配置信息
+
+session,cookie?:
+存储位置:
+    cookie存储在客户端浏览器,session存储在服务器
+存储容量:
+    cookie<=4k,一个站点最多保留20个cookie,
+    session没有上线,出于对服务器的保护,session内不可存储过多东西,并且要设置session删除机制
+存储方式:
+    cookie只能保存ASCII字符串,需要通过编码方式存储为Unicode字符或者二进制数据
+    session能存储任何类型的数据
+隐私策略:
+    cookie对客户端可见,不安全
+    session存储在服务器,安全
+有效期:
+    开发可以通过设置cookie属性,达到使cookie长期有效
+    session依赖于JESSIONID的cookie,cookie JSESSIONID的过期时间默认为-1，
+        关闭窗口该session就会失效，因而session达不到长期有效的效果
+跨域支持:
+    cookie支持跨域
+    session不支持跨域
+
+客户端禁止cookie,实现session使用:
+1.通过url重写,把sessionid作为参数追加在原url中,后续的浏览器与服务器交互中携带sessionid参数
+2.服务器返回数据包含sessionid,浏览器发送请求,携带sessionid参数
+3.通过http协议的其他header字段,服务器每次返回时设置该header字段信息,
+    浏览器中js读取该header字段,请求服务器时,js设置携带该header字段
+
+xss攻击?:
+xss(Cross Site Scripting),跨站脚本攻击,一种常见于web应用程序中的计算机安全漏洞
+预防:
+1.获取用户的输入,不用innerHtml,用innerText
+2.对用户输入过滤,如对&<>"'/等转义
---
+++ src/main/java/com/zjy/study/leetcodestudy/practice/Subject21_40/Subject_0040.java
@@ -0,0 +1,81 @@
+package com.zjy.study.leetcodestudy.practice.Subject21_40;
+
+import java.util.*;
+
+/**
+ * @Author zjy
+ * @Date 2023/2/23 9:58
+ * @Description
+ *      组合总和 II
+ */
+public class Subject_0040 {
+    /**
+     * 给定一个候选人编号的集合 candidates 和一个目标数 target ，
+     * 找出 candidates 中所有可以使数字和为 target 的组合。
+     *
+     * candidates 中的每个数字在每个组合中只能使用 一次 。
+     *
+     * 注意：解集不能包含重复的组合。 
+     *
+     * 示例 1:
+     * 输入: candidates = [10,1,2,7,6,1,5], target = 8,
+     * 输出:
+     * [
+     * [1,1,6],
+     * [1,2,5],
+     * [1,7],
+     * [2,6]
+     * ]
+     *
+     * 示例 2:
+     * 输入: candidates = [2,5,2,1,2], target = 5,
+     * 输出:
+     * [
+     * [1,2,2],
+     * [5]
+     * ]
+     */
+
+    static List<int[]> freq = new ArrayList<int[]>();
+    static List<List<Integer>> ans = new ArrayList<List<Integer>>();
+    static List<Integer> sequence = new ArrayList<Integer>();
+
+    private static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        for (int num : candidates) {
+            int size = freq.size();
+            if (freq.isEmpty() || num != freq.get(size - 1)[0]) {
+                freq.add(new int[]{num, 1});
+            } else {
+                ++freq.get(size - 1)[1];
+            }
+        }
+        dfs(0, target);
+        return ans;
+    }
+
+    private static void dfs(int pos, int rest) {
+        if (rest == 0) {
+            ans.add(new ArrayList<Integer>(sequence));
+            return;
+        }
+        if (pos == freq.size() || rest < freq.get(pos)[0]) {
+            return;
+        }
+
+        dfs(pos + 1, rest);
+
+        int most = Math.min(rest / freq.get(pos)[0], freq.get(pos)[1]);
+        for (int i = 1; i <= most; ++i) {
+            sequence.add(freq.get(pos)[0]);
+            dfs(pos + 1, rest - i * freq.get(pos)[0]);
+        }
+        for (int i = 1; i <= most; ++i) {
+            sequence.remove(sequence.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
